c08d594ca4106a52d1e5ed8c7e975c968e1091a0
hwang <hhs19922@gmail.com>
2021-03-11 01:18:19 +0900

    ?

--- MagicOnion.Client/Assets/Scripts/NewBehaviourScript.cs
+++ MagicOnion.Client/Assets/Scripts/NewBehaviourScript.cs
@@ -7,8 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+       int a = 0; 
+    }  
 
     // Update is called once per frame
     void Update()
